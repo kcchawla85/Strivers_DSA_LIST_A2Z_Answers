ec89b07c39ccfe9abb7cd245990c1f8f0e2c40bb
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-08-18 23:33:59 +0200

    Create 2_Data_Type.cpp

---
+++ 2_Data_Type.cpp
@@ -0,0 +1,18 @@
+int dataTypes(string type) {
+	if(type == "Long"){
+		return 8;
+	}
+	else if(type == "Integer"){
+		return 4;
+	}
+	else if(type == "Float"){
+		return 4;
+	}
+	else if(type == "Double"){
+		return 8;
+	}
+	else if(type == "Character"){
+		return 1;
+	}
+}
+// link: https://youtu.be/EAR7De6Goz4
