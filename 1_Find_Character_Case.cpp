fe52ed2eeb89b836bd6293406c17ce23e782c447
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-08-18 23:22:21 +0200

    Create 1_Find_Character_Case.cpp

---
+++ 1_Find_Character_Case.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char element;
+	cin>>element;
+	if(element >=65 && element<=90){
+		cout<<"1";
+	}
+	else if(element >=97 && element<=122){
+		cout<<"0";
+	}
+	else
+	{
+		cout<<"-1";
+	}
+	
+}
+// link: https://youtu.be/EAR7De6Goz4
