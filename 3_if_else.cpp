b264bb53191f20c433bb0a7536312a9ec50fe655
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-08-19 19:07:08 +0200

    Create 3_if_else.cpp

---
+++ 3_if_else.cpp
@@ -0,0 +1,13 @@
+string compareIfElse(int a, int b) {
+	if(a>b){
+		return "greater";
+
+	}
+	else if(a<b){
+		return "smaller";
+	}
+	else if(a==b){
+		return "equal";
+	}
+}
+// link: https://youtu.be/EAR7De6Goz4
