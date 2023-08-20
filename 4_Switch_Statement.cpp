234a4388438a86d0155d193c9ca881db293fb3f8
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-08-20 22:02:39 +0200

    Create 4_Switch_Statement.cpp

---
+++ 4_Switch_Statement.cpp
@@ -0,0 +1,15 @@
+double areaSwitchCase(int ch, vector<double> a) {
+	switch (ch){
+		case 1:
+		{
+			return (M_PI*a[0]*a[0]);
+		}
+		case 2:
+		{
+			return (a[0]*a[1]);
+		}
+	}
+	
+
+}
+// link: https://youtu.be/EAR7De6Goz4
