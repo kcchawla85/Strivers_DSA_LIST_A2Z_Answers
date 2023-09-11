2e60ffb37355c17272ae201b33a4a2ed1d42c8eb
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-09-11 22:35:22 +0200

    Create Pattern2.cpp

---
+++ Pattern2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for (int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<"*"<< " ";
+                
+            }
+            cout<<endl;
+        }
+    }
+};
+// link: https://practice.geeksforgeeks.org/problems/right-triangle/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_2
+// link : https://www.youtube.com/watch?v=tNm_NNSB3_w&t=91s
