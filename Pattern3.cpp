71c0cc44972e53c821b5c204dc831b36e13ed085
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-09-11 22:42:53 +0200

    Create Pattern3.cpp

---
+++ Pattern3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+// link: https://practice.geeksforgeeks.org/problems/triangle-number/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_3
+// link: https://www.youtube.com/watch?v=tNm_NNSB3_w&t=91s
