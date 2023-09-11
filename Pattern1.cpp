8e2ea715f589266b701b6a6ecbc84e5fe63f4f81
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-09-11 22:30:56 +0200

    Create Pattern1.cpp

---
+++ Pattern1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+void printSquare(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<< "*"<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+// link: https://practice.geeksforgeeks.org/problems/square-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1
+// link: https://www.youtube.com/watch?v=tNm_NNSB3_w&t=91s
