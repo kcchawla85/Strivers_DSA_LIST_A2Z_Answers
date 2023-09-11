54fc32766f3f60e068b43c961deca52df00ab513
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-09-11 22:52:51 +0200

    Create Pattern 4.cpp

---
+++ Pattern 4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+//link:https://practice.geeksforgeeks.org/problems/triangle-number-1661428795/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_4
+// https://www.youtube.com/watch?v=tNm_NNSB3_w&t=91s
