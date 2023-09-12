15b2f9068216ebfe52bae57b472779b5bbf680ae
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-09-12 23:27:40 +0200

    Create Pattern 7.cpp

---
+++ Pattern 7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                cout<<" ";
+            }
+            for(int k=0;k<2*i+1;k++){
+                cout<<"*";
+            }
+            
+            cout<<endl;
+            
+        }
+    }
+};
+// link: https://www.youtube.com/watch?v=tNm_NNSB3_w&t=1419s
+// https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
